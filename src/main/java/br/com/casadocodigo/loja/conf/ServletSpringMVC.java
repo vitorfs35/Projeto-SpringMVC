0494c19d4d7b92e5c22f8675822dd28dad39d7d2
vitorfs35 <vitorfs35@hotmail.com>
2016-11-12 15:53:00 -0200

    Versionando o código para deploy (Livro de SpringMVC - Cap.16)

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMVC.java
@@ -14,7 +14,7 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { SecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class };
+		return new Class[] { SecurityConfiguration.class, AppWebConfiguration.class, JPAConfiguration.class, JPAProductionConfiguration.class };
 	}
 
 	@Override
